12d27f9d0e45b252e0c9edc5fc70f702ee806ba4
manishimwe1 <emmymanishimwem@gmail.com>
2024-03-26 07:15:51 +0200

    initial next setup and shadcn and darkmode

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { DarkModeToggle } from "./DarkModeToggle";
+
+const Navbar = () => {
+	return (
+		<div>
+			<DarkModeToggle />
+		</div>
+	);
+};
+
+export default Navbar;
